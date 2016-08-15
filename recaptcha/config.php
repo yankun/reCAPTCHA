23b8fdea3a7acb06ef8468a02911ee41fb8bf944
cheesypoof <cheesypoof@users.noreply.github.com>
2016-08-14 23:48:48 -0700

    Add code

---
+++ recaptcha/config.php
@@ -0,0 +1,7 @@
+<?php
+    /*
+        To use reCAPTCHA, you need to sign up for an API key pair for your site.
+        Please visit https://www.google.com/recaptcha/admin to get the keys and enter them below.
+    */
+    define( 'K_RECAPTCHA_SITE_KEY', '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI' );
+    define( 'K_RECAPTCHA_SECRET_KEY', '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe' );
